67eca8cee461b790e17f330bb5dc89ad848e7c49
Sindhuja <dsindhujasri.vspm@gmail.com>
2022-11-15 16:14:06 +0530

    Practice Java Maven

---
+++ src/main/java/in/kmit/Practice_JM/App.java
@@ -0,0 +1,13 @@
+package in.kmit.Practice_JM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the practice project" );
+    }
+}
